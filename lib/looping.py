429322b82c9b7f9a2923d7b1d7686ebc6ca90619
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-03-20 12:29:12 -0400

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+
+    # for number in range (10, 0, -1)
+        # print(number)
+        # print ("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [x ** 2 for x in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
